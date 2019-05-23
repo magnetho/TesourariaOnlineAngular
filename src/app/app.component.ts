bf2ad8045e794bd8ba1db7bd026b38d21ccc24c3
Lucas Rodrigues <lucas.silva@iatec.com>
2019-05-23 15:50:44 -0300

    Idioma em cache
    Guarda em cache para, ao recarregar a página, estar no último idioma selecionado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,7 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'TesourariaOnlineAngular';
-  public language = navigator.language;
-  constructor(private translate: TranslateService) {
-    if (this.language != 'en' 
-      && this.language != 'pt' 
-      && this.language != 'es'
-      && this.language != 'en-US'
-      && this.language != 'pt-BR'
-      && this.language != 'es-ES'){
-      translate.setDefaultLang('en-US');
-    } else {
-      translate.setDefaultLang(this.language);
-    }
-  }
-
-  public switchLanguage(language: string) {
-    this.translate.use(language);
-  }  
+  title = 'TesourariaOnlineAngular';  
 }
 
   
